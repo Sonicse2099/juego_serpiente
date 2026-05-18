5c8af718e8d51a4201849b450b257c9b0e106248
Sonicse2099 <joseluissonico@hotmail.com>
2026-05-18 17:17:46 -0500

    Serpiente de color diferenste

--- serpiente.js
+++ serpiente.js
@@ -3,27 +3,52 @@ const ctx = canvas.getContext("2d");
 
 const TAMANIO_CELDA = 25;
 
-dibujarTodo();
-
+const serpiente = [
+    {x: 0, y: 5},
+    {x: 0, y: 6},
+    {x: 0, y: 7},
+    {x: 0, y: 8},
+    {x: 0, y: 9}
+];
 
 function limpiarCanvas() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
+function pintarParte(lineaX, lineaY, color) {
+    const x = lineaX * TAMANIO_CELDA;
+    const y = lineaY * TAMANIO_CELDA;
+
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, TAMANIO_CELDA, TAMANIO_CELDA);
+
+    ctx.strokeStyle = "#111827";
+    ctx.strokeRect(x, y, TAMANIO_CELDA, TAMANIO_CELDA);
+}
+
+function pintarSerpiente() {
+    for (let i = 0; i < serpiente.length; i++) {
+        if (i === 0) {
+            pintarParte(serpiente[i].x, serpiente[i].y, "#facc15"); // amarillo
+        } else {
+            pintarParte(serpiente[i].x, serpiente[i].y, "#ef4444"); // rojo
+        }
+    }
+}
+
 function dibujarTablero() {
     ctx.strokeStyle = "#1e293b";
     ctx.lineWidth = 0.5;
-
     for (let x = 0; x <= canvas.width; x += TAMANIO_CELDA) {
         ctx.beginPath();
-        ctx.moveTo(x, 0);               
-        ctx.lineTo(x, canvas.height);   
+        ctx.moveTo(x, 0);
+        ctx.lineTo(x, canvas.height);
         ctx.stroke();
     }
     for (let y = 0; y <= canvas.height; y += TAMANIO_CELDA) {
         ctx.beginPath();
-        ctx.moveTo(0, y);               
-        ctx.lineTo(canvas.width, y);   
+        ctx.moveTo(0, y);
+        ctx.lineTo(canvas.width, y);
         ctx.stroke();
     }
 }
@@ -31,7 +56,10 @@ function dibujarTablero() {
 function dibujarTodo() {
     limpiarCanvas();
     dibujarTablero();
+    pintarSerpiente();
 }
+
+dibujarTodo();
 
 
 
